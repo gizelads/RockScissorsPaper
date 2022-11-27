f5e2285420da405f3be9796506464024821e6af2
GizelaDS <gizelads.me@gmail.com>
2022-11-26 23:39:39 -0400

    creación mokepon.js y seleccion de mascota mediante boton

---
+++ js/mokepon.js
@@ -0,0 +1,6 @@
+function seleccionarMascotaJugador(){
+    alert('Seleccionaste tu mascota')
+}
+
+let botonMascota = document.getElementById('boton-mascota')
+botonMascota.addEventListener('click', seleccionarMascotaJugador)
