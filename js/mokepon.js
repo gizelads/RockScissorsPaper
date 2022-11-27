d594989a96cd477935a260a78f9e1cb3b3a04ccb
GizelaDS <gizelads.me@gmail.com>
2022-11-27 12:22:27 -0400

    identificacion de seleccion de mascota del jugador

--- js/mokepon.js
+++ js/mokepon.js
@@ -4,7 +4,28 @@ function iniciarJuego() {
 }
 
 function seleccionarMascotaJugador() {
-    alert('Seleccionaste tu mascota')
+    let hipodogeInput = document.getElementById('hipodoge')
+    let capipepoInput = document.getElementById('capipepo')
+    let ratigueyaInput = document.getElementById('ratigueya')
+    let langostelvisInput = document.getElementById('langostelvis')
+    let tucapalmaInput = document.getElementById('tucapalma')
+    let pydosInput = document.getElementById('pydos')
+
+    if(hipodogeInput.checked) {
+        alert("Ha seleccionado Hipodoge💧")
+    } else if(capipepoInput.checked) {
+        alert("Ha seleccionado Capipepo🌱")
+    } else if(ratigueyaInput.checked) {
+        alert("Ha seleccionado Ratigueya🔥")
+    } else if(langostelvisInput.checked) {
+        alert("Ha seleccionado Langostelvis💧🔥")
+    } else if(tucapalmaInput.checked) {
+        alert("Ha seleccionado Tucapalma💧🌱")
+    } else if(pydosInput.checked) {
+        alert("Ha seleccionado Pydos🔥🌱")
+    } else {
+        alert("Debe seleccionar una mascota")
+    }
 }
 
 window.addEventListener('load', iniciarJuego)
