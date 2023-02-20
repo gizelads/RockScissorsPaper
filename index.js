aaa415d6ab4746f38ec786b2211ac65733cb2d59
GizelaDS <74159414+gizelads@users.noreply.github.com>
2023-02-20 13:39:52 -0400

    servidor expressjs

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log("Hola, Node!")
+const express = require("express") // importamos express
+const app = express() // creamos una app con expressjs
+
+app.get("/", (req, res) => { // indicamos que cuando responda "Hola" cuando hay una peticion a la direccion raiz "/"
+    res.send("Hola, Express")
+})
+
+app.listen(8080, () => { // indicamos que el servidor escuche continuamente en el puerto 8080 las peticiones de los clientes
+    console.log("Servidor funcionando")
+})
