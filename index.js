f17af66710d99c8bafe7e8134c283535681645c3
GizelaDS <74159414+gizelads@users.noreply.github.com>
2023-02-20 20:05:45 -0400

    api rest expressjs, id de jugador

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
-const express = require("express") // importamos express
-const app = express() // creamos una app con expressjs
+const express = require("express")
+const app = express()
 
-app.get("/", (req, res) => { // indicamos que cuando responda "Hola" cuando hay una peticion a la direccion raiz "/"
-    res.send("Hola, Express")
+const jugadores = []
+
+class Jugador {
+    constructor(id) {
+        this.id = id
+    }
+}
+
+app.get("/unirse", (req, res) => {
+    const id = `${Math.random()}`
+    const jugador = new Jugador(id)
+    jugadores.push(jugador)
+
+    res.setHeader("Access-Control-Allow-Origin", "*")
+    res.send(id)
 })
 
-app.listen(8080, () => { // indicamos que el servidor escuche continuamente en el puerto 8080 las peticiones de los clientes
+app.listen(8080, () => {
     console.log("Servidor funcionando")
 })
--- js/characters.js
+++ js/characters.js
@@ -216,7 +216,22 @@ window.addEventListener("load", () => {   //iniciar juego
     botonPersonajevsRandom.addEventListener("click", seleccionarPersonajeJugadorvsRandom)
     botonPersonajevsMap.addEventListener("click",  seleccionarPersonajeJugadorvsMap)
     botonReiniciar.addEventListener("click", reiniciar)
+
+    unirseAlJuego()
 })
+
+function unirseAlJuego() {
+    fetch("http://localhost:8080/unirse")
+        .then(function (res) {
+            // console.log(res)
+            if (res.ok) {
+                res.text()
+                    .then(function (respuesta) {
+                        console.log(respuesta)
+                    })
+            }
+        })
+}
 
 function seleccionarPersonajeJugadorvsRandom() {
     personajeSeccion.style.display = "none"
